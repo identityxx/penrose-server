27db2d9cdc229fcdb0eb78ca3bb59a222eea41b8
endisd <endisd@04c32f3d-caf7-0310-9fe0-d49f5cee804d>
2006-12-09 08:17:55 +0000

    Updated Java Backend implementation.
    git-svn-id: http://unused-160-33.aus.redhat.com/svn/penrose-server/trunk@726 04c32f3d-caf7-0310-9fe0-d49f5cee804d

---
+++ server/src/java/org/safehaus/penrose/backend/PenroseBackend.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2000-2006, Identyx Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.safehaus.penrose.backend;
+
+import java.util.*;
+import java.io.File;
+
+import org.safehaus.penrose.Penrose;
+import org.safehaus.penrose.server.PenroseServer;
+import org.safehaus.penrose.config.PenroseConfig;
+import org.safehaus.penrose.service.ServiceConfig;
+import org.safehaus.backend.Backend;
+import org.safehaus.backend.Session;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+import com.novell.ldap.LDAPException;
+
+/**
+ * @author Endi S. Dewata
+ */
+public class PenroseBackend implements Backend {
+
+    Logger log = LoggerFactory.getLogger(getClass());
+
+    public String home;
+
+    public PenroseServer penroseServer;
+
+    public Map sessions = new HashMap();
+
+    public PenroseBackend() {
+        home = System.getProperty("penrose.home");
+
+        File f = new File((home == null ? "" : home+File.separator)+"conf"+File.separator+"log4j.xml");
+        if (f.exists()) DOMConfigurator.configure(f.getAbsolutePath());
+    }
+
+    /**
+     * Initialize Penrose engine.
+     *
+     * @return return code
+     * @throws Exception
+     */
+    public int init() throws Exception {
+        try {
+            return initImpl();
+        } catch (Throwable e) {
+            log.error(e.getMessage(), e);
+            return LDAPException.OPERATIONS_ERROR;
+        }
+    }
+
+    public int initImpl() throws Exception {
+
+        penroseServer = new PenroseServer(home);
+
+        PenroseConfig penroseConfig = penroseServer.getPenroseConfig();
+
+        ServiceConfig ldapServiceConfig = penroseConfig.getServiceConfig("LDAP");
+        ldapServiceConfig.setEnabled(false);
+
+        penroseServer.start();
+
+        return LDAPException.SUCCESS;
+    }
+
+    /**
+     * Get session.
+     * 
+     * @param connectionId
+     * @return session
+     */
+    public Session getSession(int connectionId) throws Exception {
+        return (PenroseSession)sessions.get(new Integer(connectionId));
+    }
+
+    /**
+     * Create connection.
+     * 
+     * @param connectionId
+     */
+    public Session createSession(int connectionId) throws Exception {
+        log.debug("openConnection("+connectionId+")");
+        Penrose penrose = penroseServer.getPenrose();
+        PenroseSession session = new PenroseSession(penrose.newSession());
+        if (session == null) throw new Exception("Unable to create session.");
+        sessions.put(new Integer(connectionId), session);
+        return session;
+    }
+
+    /**
+     * Remove connection.
+     * 
+     * @param connectionId
+     */
+    public void closeSession(int connectionId) throws Exception {
+        log.debug("closeConnection("+connectionId+")");
+        PenroseSession session = (PenroseSession)sessions.remove(new Integer(connectionId));
+        if (session != null) session.close();
+    }
+
+}
--- server/src/java/org/safehaus/penrose/backend/PenroseResults.java
+++ server/src/java/org/safehaus/penrose/backend/PenroseResults.java
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-package org.safehaus.penrose.openldap;
+package org.safehaus.penrose.backend;
 
 import org.safehaus.backend.Results;
 import org.safehaus.penrose.session.PenroseSearchResults;
